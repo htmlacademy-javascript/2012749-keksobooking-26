492b0476f6cf4dd79787165b6fde0ada27be4db8
lilgikki <katy.kostyleva2010@gmail.com>
2022-07-15 23:52:29 +0300

    made validation with pristine

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,42 +1,46 @@
 // eslint-disable-next-line no-unused-vars
 import { form } from './form-status.js';
 
+const pristineForm = new Pristine(form, {
+  classTo: 'ad-form__element', // Элемент, на который будут добавляться классы
+  errorClass: 'ad-form__element--invalid', // Класс, обозначающий невалидное поле
+  successClass: 'ad-form__element--valid', // Класс, обозначающий валидное поле
+  errorTextParent: 'ad-form__element', // Элемент, куда будет выводиться текст с ошибкой
+  errorTextTag: 'span', // Тег, который будет обрамлять текст ошибки
+  errorTextClass: 'ad-form__error' // Класс для элемента с текстом ошибки
+});
+
 const titleForm = form.querySelector('#title');
 const priceForm = form.querySelector('#price');
 const roomNumForm = form.querySelector('#room_number');
 const capacityForm = form.querySelector('#capacity');
 const MIN_TITLE_LENGTH = 30;
 const MAX_TITLE_LENGTH = 100;
-const MAX_PRICE_LENGHT = 100000;
-
-const validateTitle = () => {
-  if (titleForm.value.length < MIN_TITLE_LENGTH || titleForm.value.length > MAX_TITLE_LENGTH) {
-    titleForm.setCustomValidity(`Минимальная длина: ${MIN_TITLE_LENGTH} символов. Максимальная длина: ${MAX_TITLE_LENGTH} символов.`);
-  } else {titleForm.setCustomValidity('');}
-  titleForm.reportValidity();
+const MAX_PRICE_VALUE = 100000;
+const roomsCapacity = {
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3'],
+  '100': ['0'],
 };
 
-const validatePrice = () => {
-  if (priceForm.value > MAX_PRICE_LENGHT) {
-    priceForm.setCustomValidity(`Максимальная цена за ночь: ${MAX_PRICE_LENGHT}.`);
-  } else {priceForm.setCustomValidity('');}
-  priceForm.reportValidity();
-};
+const validateTitle = () => titleForm.value.length >= MIN_TITLE_LENGTH && titleForm.value.length <= MAX_TITLE_LENGTH;
 
-const validateCapacityNum = () => {
-  if (roomNumForm.value === '1' && capacityForm.value !== '1') {
-    capacityForm.setCustomValidity('1 комната - 1 гость');
-  } else if (roomNumForm.value === '2' && capacityForm.value !== '1' && capacityForm.value !== '2') {
-    capacityForm.setCustomValidity('2 комнаты - от 1 до 2 гостей');
-  } else if (roomNumForm.value === '3' && capacityForm.value === '0') {
-    capacityForm.setCustomValidity('3 комнаты - от 1 до 3 гостей');
-  } else if (roomNumForm.value === '100' && capacityForm.value !== '0') {
-    capacityForm.setCustomValidity('100 комнат - не для гостей');
-  } else {capacityForm.setCustomValidity('');}
-  capacityForm.reportValidity();
-};
+const validatePrice = () => priceForm.value <= MAX_PRICE_VALUE;
 
-titleForm.addEventListener('input', validateTitle);
-priceForm.addEventListener('input', validatePrice);
-roomNumForm.addEventListener('change', validateCapacityNum);
-capacityForm.addEventListener('change', validateCapacityNum);
+const validateCapacityNum = () => roomsCapacity[roomNumForm.value].includes(capacityForm.value);
+
+pristineForm.addValidator(titleForm, validateTitle, `От ${MIN_TITLE_LENGTH} до ${MAX_TITLE_LENGTH} символов.`);
+pristineForm.addValidator(priceForm, validatePrice, `Максимальная цена за ночь ${MAX_PRICE_VALUE}.`);
+pristineForm.addValidator(roomNumForm, validateCapacityNum, 'Количество гостей не сответствует количеству комнат');
+
+function onUnitChange () {
+  pristineForm.validate(roomNumForm);
+}
+
+[roomNumForm, capacityForm].forEach((item) => item.addEventListener('change', onUnitChange));
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristineForm.validate();
+});
